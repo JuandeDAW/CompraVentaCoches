738502bedffca7972eb13850ba7097eec50b9920
pablohernandez27 <pablohernandezbernal2@gmail.com>
2024-05-28 13:13:48 +0200

    Solucion errores

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,10 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+     
+    build: {
+        rollupOptions: {
+          external: ['google-charts'], 
+        },
+      },
 });
